b81bbc84026f858113e7da3d7dc1edb1da04ab25
elxp101t3r <eliotx10101@outlook.com>
2023-11-12 10:36:44 +0200

    Final touches

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+import os
 
 question_bank = []
 for i in range(len(question_data)):
     question_bank.append(Question(question_data[i]['text'], question_data[i]['answer']))
-
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+os.system('clear')
+print("You've completed the quiz!!!\n")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
