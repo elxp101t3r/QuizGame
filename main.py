7e092b9a18456afb73bbfd95c123b800264ede59
elxp101t3r <eliotx10101@outlook.com>
2023-11-12 11:04:56 +0200

    Completed!

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for i in range(len(question_data)):
-    question_bank.append(Question(question_data[i]['text'], question_data[i]['answer']))
+    question_bank.append(Question(question_data[i]['question'], question_data[i]['correct_answer']))
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
