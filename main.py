bdab9a7378d85747ff2cb17035b8b9354e509ce5
elxp101t3r <eliotx10101@outlook.com>
2023-11-12 10:39:19 +0200

    99.9% completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
-import os
+# import os
 
 question_bank = []
 for i in range(len(question_data)):
@@ -9,6 +9,6 @@ for i in range(len(question_data)):
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
-os.system('clear')
+# os.system('clear')
 print("You've completed the quiz!!!\n")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
