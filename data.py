eb9258f726b89bbed6ca47431593280634c8bbfa
elxp101t3r <eliotx10101@outlook.com>
2023-11-12 11:52:36 +0200

    request data in any category

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import requests as r
 amount_of_questions = '10'
-response = r.get(f'https://opentdb.com/api.php?amount={amount_of_questions}&category=32&type=boolean')
+type_of = 'boolean'
+response = r.get(f'https://opentdb.com/api.php?amount={amount_of_questions}&type={type_of}')
 question_data = response.json()
